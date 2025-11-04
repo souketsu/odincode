2a99f84479dc1dff4c4138481f10bd6452a87e5c
souketsu <sou_ketsu@foxmail.com>
2025-11-04 09:01:48 +0000

    完成flexbox的落地项目的构建

---
+++ odinhtml/JavaScript/javascript.js
@@ -0,0 +1,34 @@
+let myname = "caojey";
+console.log(myname);
+
+myname = "caojey123";
+console.log(myname);
+
+const pi = 3.1415926;
+let radius = 5;
+// pi = 3.14;
+console.log(pi+radius);
+console.log(pi*radius*radius);
+console.log(3 + 2 - 76 * (1 + 1));
+
+// Variables and Operators 作业
+
+console.log(23 + 97);
+console.log(12 + 354 + 55 + 748 + 47 + 23)
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+let b = 7 * a;
+console.log(a);
+console.log(b);
+a = a + 10;
+console.log(b);
+
+let max = 57;
+console.log(max);
+let actual = max -13;
+console.log(actual);
+let percentage = actual / max
+console.log(percentage);
+console.log("sddd" + 100);
+
