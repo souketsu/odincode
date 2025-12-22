6fdda22bcaec88439e7751b922a46cac149e7bdd
souketsu <sou_ketsu@foxmail.com>
2025-12-22 22:36:28 +0800

    对象构造函数

---
+++ odinhtml/JavaScript/ObjectConstructors.js
@@ -0,0 +1,12 @@
+function Book(title,info,author,pages,read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, read: ${this.read}`;
+  }
+}
+
+const wuwaizhuan = new Book('武林外传','武林外传info','作者1',100,true);
+console.log(wuwaizhuan.info());
